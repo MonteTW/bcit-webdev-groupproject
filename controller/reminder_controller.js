8588c1ac9ac8c0388daaad1f1a16a0daeb79a0ef
monte <mkt.monte@gmail.com>
2024-12-03 20:22:48 -0800

    complete view single reminder and delete a reminder

--- controller/reminder_controller.js
+++ controller/reminder_controller.js
@@ -14,16 +14,21 @@ let remindersController = {
     res.render("reminder/create");
   },
 
-  listOne: (req, res) => {
-    let index_user = database.findIndex((user) => user.id === req.user.id)
-    let reminderToFind = req.params.id;
-    let searchResult = database[index_user].reminders.find(function (reminder) {
-      return reminder.id == reminderToFind;
-    });
+  listOne: async (req, res) => {
+    const reminders = await db.reminder.findMany({
+      where: { userId: req.user.id }
+    })
+    let reminderToFind = Number(req.params.id);
+    let searchResult = await db.reminder.findUnique({
+      where: { 
+        userId: req.user.id,
+        id: reminderToFind
+      }
+    })
     if (searchResult != undefined) {
       res.render("reminder/single-reminder", { reminderItem: searchResult });
     } else {
-      res.render("reminder/index", { reminders: database.cindy.reminders });
+      res.render("reminder/index", { reminders:reminders });
     }
   },
 
@@ -72,12 +77,15 @@ let remindersController = {
     // implement this code
   },
 
-  delete: (req, res) => {
+  delete: async (req, res) => {
     // Implement this code
-    let index_user = database.findIndex((user) => user.id === req.user.id)
-    let reminderToFind = req.params.id;
-    let index_reminder = database[index_user].reminders.findIndex((reminder) => reminder.id == reminderToFind);
-    database[index_user].reminders.splice(index_reminder,1)
+    let reminderToFind = Number(req.params.id);
+    await db.reminder.delete({
+      where: { 
+        userId: req.user.id,
+        id: reminderToFind
+      }
+    })
     res.redirect("/reminders")
   },
 };
